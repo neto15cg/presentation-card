ec2aa3ee9e7ce8af22f829fb251c0000f9bbacd7
Eliezer Marques <neto15.cg@gmail.com>
2020-05-16 19:47:19 -0300

    Create skeleton

--- pages/index.js
+++ pages/index.js
@@ -1,193 +1,212 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { MdDeveloperMode } from "react-icons/md";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Eliezer Marques</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main className="container">
+        <h1 className="title">Eliezer Marques</h1>
+        <div className="content">
+          <div className="presentation-container">
+            <div className="presentation">
+              <p className="description">
+                I am a computer engineer, currently working at &nbsp;
+                <a
+                  className="link"
+                  target="_blank"
+                  href="https://www.agilize.com.br"
+                >
+                  Agilize Contabilidade
+                </a>
+                &nbsp;as a Front-end developer, but I also have experience as a
+                Back-end developer.
+              </p>
+            </div>
+            <MdDeveloperMode color="#fff" size={300} />
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <div className="network">
+              <img
+                className="profile"
+                src="/profile.png"
+                width={60}
+                alt="Eliezer Marques"
+              />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <a
+                className="link-icon"
+                target="_blank"
+                href="https://br.linkedin.com/in/eliezer-marques-623b95124"
+              >
+                Linkedin &nbsp;
+                <FaLinkedin color="#fff" size={25} />
+              </a>
+              &nbsp;&nbsp;&nbsp;
+              <a
+                className="link-icon"
+                target="_blank"
+                href="https://github.com/neto15cg"
+              >
+                Github &nbsp;
+                <FaGithub color="#fff" size={25} />
+              </a>
+            </div>
+          </div>
+          <div className="skill-container">
+            <div className="card">
+              <div className="card-content">
+                <p>JAVASCRIPT / TYPESCRIPT</p>
+                <h3>
+                  I have been working as a JAVASCRIPT developer for 3 years. The
+                  frameworks I use most are ReactJS and React Native.
+                </h3>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-content">
+                <p>PHP</p>
+                <h3>I'm working with PHP during 3 years.</h3>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-content">
+                <p>GOLANG</p>
+                <h3>I'm working with GOLANG during 2 years.</h3>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+      <footer></footer>
 
       <style jsx>{`
         .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
           display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
           flex: 1;
-          display: flex;
+          align-items: center;
           flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
+          background-color: #151515;
+          height: 100vh;
+          overflow: hidden;
         }
 
         .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
+          font-size: 48px;
+          color: #ffffff;
+          line-height: 1.25;
+          font-weight: 700;
+          font-family: Roboto;
           text-align: center;
+          width: 100%;
+          margin: 0;
+          padding: 30px 30px 30px 30px;
         }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
+        .content {
+          display: flex;
+          flex: 1;
+          flex-direction: row;
+          width: 100%;
         }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        .presentation-container {
+          display: flex;
+          flex: 1;
+          flex-direction: column;
+          width: 100%;
+          align-items: center;
+          justify-content: space-between;
+          padding: 30px 30px 30px 30px;
+          max-height: 100%;
         }
 
-        .grid {
+        .presentation {
+          max-width: 370px;
+        }
+
+        .network {
+          display: flex;
+          flex-direction: row;
+          max-width: 370px;
+          width: 100%;
+        }
+
+        .profile {
+          border-radius: 50%;
+        }
+
+        .link {
+          color: #ffffff;
+        }
+
+        .link-icon {
           display: flex;
           align-items: center;
           justify-content: center;
-          flex-wrap: wrap;
+          text-decoration: none;
+          color: #ffffff;
+        }
 
-          max-width: 800px;
-          margin-top: 3rem;
+        .description {
+          color: #949495;
+          font-family: Roboto;
+          font-size: 16px;
+        }
+
+        .skill-container {
+          max-height: calc(100vh - 120px);
+          overflow: auto;
+          display: flex;
+          flex: 1;
+          flex-direction: column;
+          align-items: center;
+          padding: 30px 30px 30px 30px;
+        }
+
+        ::-webkit-scrollbar {
+          width: 0;
         }
 
         .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
+          width: 500px;
+          min-height: 200px;
+          background-color: #202022;
+          transition: all 0.2s ease-in-out;
+          margin: 5px 0;
         }
 
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
+        .card:hover {
+          transform: scale(1.1);
         }
 
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
+        .card-content {
+          flex: 1;
+          height: 100%;
+          padding: 40px;
         }
 
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
+        .card-content p {
+          letter-spacing: 3px;
+          color: #ffffff;
+          font-weight: 400;
+          font-size: 16px;
+        }
+        .card-content h3 {
+          color: #ffffff;
+          font-weight: 400;
+          font-size: 16px;
         }
 
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
+        // @media only screen and (max-width: 600px) {
+        //   .container {
+        //     flex-direction: column;
+        //   }
+        // }
       `}</style>
 
       <style jsx global>{`
@@ -205,5 +224,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
